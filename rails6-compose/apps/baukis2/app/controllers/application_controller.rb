508a49c2fa187c78fb83edfce29bdb32ffe1f3b2
kenji7157 <kawake.kk@gmail.com>
2022-02-21 02:20:00 +0900

    chap6 fin

--- rails6-compose/apps/baukis2/app/controllers/application_controller.rb
+++ rails6-compose/apps/baukis2/app/controllers/application_controller.rb
@@ -2,12 +2,9 @@ class ApplicationController < ActionController::Base
   layout :set_layout
 
   class Forbidden < ActionController::ActionControllerError; end
-  class IpAddressRejected < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end  
 
-  rescue_from StandardError, with: :rescue500
-  rescue_from Forbidden, with: :rescue403
-  rescue_from IpAddressRejected, with: :rescue403
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  include ErrorHandlers if Rails.env.production?
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|cutomer)/})
@@ -15,21 +12,6 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
-  end
-
-
-  private def rescue403(e)
-    @exception = e
-    render "errors/forbidden", status: 403
-  end
-
-  private def rescue404(e)
-    @exception = e
-    render "errors/not_found", status: 403
-  end
-
-  private def rescue500(e)
-    render "errors/internal_server_error", status: 500
   end
 
 end
