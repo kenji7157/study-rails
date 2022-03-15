39f5fdc992588525f7825055d7dcd39ba303b50a
kenji7157 <kawake.kk@gmail.com>
2022-03-15 10:27:50 +0900

    chap13-3

--- rails6-compose/apps/baukis2/app/controllers/admin/staff_events_controller.rb
+++ rails6-compose/apps/baukis2/app/controllers/admin/staff_events_controller.rb
@@ -2,9 +2,10 @@ class Admin::StaffEventsController < Admin::Base
   def index
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occured_at: :desc)
+      @events = @staff_member.events.order(occurred_at: :desc)
     else
-      @events = StaffEvent.order(occured_at: :desc)
+      @events = StaffEvent.order(occurred_at: :desc)
     end
+    @events = @events.page(params[:page])
   end
 end
